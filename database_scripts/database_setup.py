c142af7c2ceef2a7247576b2462db515812f028e
Jose Ricardo Pacheco <jrp2022@gmail.com>
2024-10-30 14:26:03 -0600

    Minor changes

--- database_scripts/database_setup.py
+++ database_scripts/database_setup.py
@@ -1,10 +1,14 @@
-# database_setup.py
+"""
+Database set up for connecting to the database with ssh keys, to the remote server. 
+Refer to the config.py file, localted in the root of the project config.py
+Along with proper .env file set up refer to the template_env file in the root of the project.
+"""
 from config import Base, engine, close_tunnel
 
 def init_db():
     Base.metadata.create_all(bind=engine)
-    print("Database tables created successfully.")
+    print("Database Initilialize correctly.")
 
 if __name__ == "__main__":
     init_db()
-    close_tunnel()  # Make sure to close the tunnel after running the script
+    close_tunnel()  # At the end of the code we close the SSH tunnel
